6cc89ba28375a6e19ed4b8d687051daaf96e2efc
Ayush Maurya <ayushayushayushmaurya@gmail.com>
2025-12-20 12:14:02 +0530

    first commit

---
+++ app/models/product.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose'
+
+const ProductSchema = new mongoose.Schema({
+    // product_id:Number,
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    stock: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    category: {
+        type: String,
+        required: true
+    },
+
+    images: {
+        type: [String],
+        required: true
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    }
+},
+    {
+        timestamps: true
+    }
+)
+
+const Product = mongoose.model("Product", ProductSchema) || mongoose.models.Product
+export default Product
---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <div className="navbar bg-green-300 h-15">
+    
+    </div>
+   
+    </>
+  );
+}
