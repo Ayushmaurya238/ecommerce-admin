a12af2c1ab7769b6e6c9c859d794f38bdc6a908f
Ayush Maurya <ayushayushayushmaurya@gmail.com>
2025-12-20 20:00:04 +0530

    expanded routes

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -3,8 +3,8 @@ import clientPromise from "@/lib/mongodb";
 export async function GET(req) {
     // let data=await fetch('/');
     // console.log(req)
-    return NextResponse.json({success:true,req})
-    
+    return NextResponse.json({ success: true, req })
+
 }
 /*req.body=
 {
@@ -23,11 +23,22 @@ timestamp:updated or lauchtime fas..
 
 
 export async function POST(req) {
-    let data=await req.json();
-    const client=await clientPromise;
-    const db=client.db('ecommerceDB')
-    const products=db.collection('products')
-    
+    let data = await req.json();
+    const client = await clientPromise;
+    const db = client.db('ecommerceDB')
+    const products = db.collection('products')
+    let product = products.findOne({ name: data.name, description: data.description, sellerId: data.sellerId });
+    if (product) {
+        NextResponse.json({ success: false, message: 'You have already added that product for sale. You can update the product properties by their id' })
+    }
+    else{
+        products.insertOne({
+            sellerId:1,
+            name:data.name,
+            
+        })
+        NextResponse.json({ success: true, message: 'Product added for sale' })
+    }
 
 
 }
--- models/product.js
+++ models/product.js
@@ -2,6 +2,11 @@ import mongoose from 'mongoose'
 
 const ProductSchema = new mongoose.Schema({
     // product_id:Number,
+    sellerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Seller",
+      required: true,
+    },
     name: {
         type: String,
         required: true,
