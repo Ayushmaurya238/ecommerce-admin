ad4eb3b2f39d1727552d8a57f84327d9e66e0ef6
Ayush Maurya <ayushayushayushmaurya@gmail.com>
2026-01-01 22:12:32 +0530

    fsdfafd

--- app/api/auth/logout/route.js
+++ app/api/auth/logout/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 export async function GET() {
-  const response = NextResponse.redirect(new URL('/', process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000'));
+  const response = NextResponse.redirect(new URL('/', process.env.NEXT_PUBLIC_APP_URL || 'https://ecommerce-admin-three-eta.vercel.app/'));
 
   response.cookies.set('token', '', {
     httpOnly: true,
